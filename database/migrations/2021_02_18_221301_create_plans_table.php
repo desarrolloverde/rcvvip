299c53bdfe402b7cf0057a9225d33a67d13b3d0d
desarrolloverde <vargasoz.sr@gmail.com>
2021-02-26 18:18:12 -0400

    acutualizacion de la tabla plan: manejo de cotizacion

--- database/migrations/2021_02_18_221301_create_plans_table.php
+++ database/migrations/2021_02_18_221301_create_plans_table.php
@@ -21,6 +21,13 @@ class CreatePlansTable extends Migration
             $table->smallInteger('activo');
             $table->decimal('costo', $precision = 10, $scale = 2);
             $table->decimal('cobertura', $precision = 15, $scale = 2);
+            $table->decimal('costo_asi_med', $precision = 10, $scale = 2);
+            $table->decimal('costo_invalidez', $precision = 10, $scale = 2);
+            $table->decimal('costo_fallec', $precision = 10, $scale = 2);
+            $table->decimal('costo_bienes', $precision = 10, $scale = 2);
+            $table->decimal('costo_grua', $precision = 10, $scale = 2);
+            $table->decimal('costo_estacionamiento', $precision = 15, $scale = 2);
+            $table->decimal('costo_cobertura', $precision = 15, $scale = 2);
             $table->timestamps();
         });
     }
